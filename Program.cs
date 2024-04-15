3c8883701ef284fcb2d5b0479f4335123ea28c0f
Wufferman <eaajbst@students.eaaa.dk>
2024-04-15 13:14:48 +0200

    Vault Changes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
 
 var builder = WebApplication.CreateBuilder(args);
-string mySecret = Environment.GetEnvironmentVariable("Secret") ?? "none";
-string myIssuer = Environment.GetEnvironmentVariable("Issuer") ?? "none";
+
+var EndPoint = Environment.GetEnvironmentVariable("VAULT_IP");
+var httpClientHandler = new HttpClientHandler();
+httpClientHandler.ServerCertificateCustomValidationCallback =
+(message, cert, chain, sslPolicyErrors) => { return true; };
+
+
+IAuthMethodInfo authMethod =
+new TokenAuthMethodInfo(Environment.GetEnvironmentVariable("VAULT_SECRET"));
+// Initialize settings. You can also set proxies, custom delegates etc. here.
+var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+{
+    Namespace = "",
+    MyHttpClientProviderFunc = handler
+    => new HttpClient(httpClientHandler)
+    {
+        BaseAddress = new Uri(EndPoint)
+    }
+};
+IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+// Use client to read a key-value secret.
+Secret<SecretData> kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2
+.ReadSecretAsync(path: "jwt", mountPoint: "secret");
+var jwtSecret = kv2Secret.Data.Data["secret"];
+var jwtIssuer = kv2Secret.Data.Data["issuer"];
+
+string mySecret = (string)jwtSecret ?? "none";
+string myIssuer = (string)jwtIssuer ?? "none";
 builder.Services
 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
